1d63c1124eac0db03b0201a7752459c3d0c9c587
Chitrakar09 <chitrakarpratyush@gmail.com>
2025-10-23 11:06:23 +0545

    changes on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
         <span className='font-bold text-sm lg:text-lg'>Pratyush Chitrakar</span>
         <span className='text-[0.625rem] lg:text-[0.7rem] flex flex-wrap'>© 2025 • Designed & Developed by Pratyush Chitrakar</span>
       </div>
-      <div id="follow" className='flex flex-col md:flex-row md:justify-end md:items-center items-end justify-center w-full md:gap-2'>
-        <span className='text-[0.75rem] font-bold md:text-lg lg:text-xl'>Follow</span>
+      <div id="follow" className='flex flex-col md:flex-row md:justify-end md:items-end items-end justify-center w-full md:gap-2'>
+        <span className='text-[0.625rem] md:text-sm lg:text-lg font-bold'>Follow</span>
         <span className='text-[0.625rem] md:text-sm lg:text-lg hover:text-accent-amber hover:-translate-y-0.5 transition ease-in-out'><a href=''>Github</a></span>
         <span className='text-[0.625rem] md:text-sm lg:text-lg hover:text-accent-amber hover:-translate-y-0.5 transition ease-in-out'><a href=''>Linkedin</a></span>
         <span className='text-[0.625rem] md:text-sm lg:text-lg hover:text-accent-amber hover:-translate-y-0.5 transition ease-in-out'><a href=''>Instagram</a></span>
