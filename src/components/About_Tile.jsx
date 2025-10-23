d55563e423c5bb2b6c9159f51289844f00f28673
Chitrakar09 <chitrakarpratyush@gmail.com>
2025-10-23 18:22:21 +0545

    added things in about me page

--- src/components/About_Tile.jsx
+++ src/components/About_Tile.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ProfilePic from './ProfilePic'
+import { css, tailwind, html, react, javascript, git, figma } from '@/assets/icon_index'
 
 function About_Tile() {
+    const icons = [
+        { icon: react },
+        { icon: javascript },
+        { icon: tailwind },
+        { icon: html },
+        { icon: css },
+        { icon: figma },
+        { icon: git },
+    ]
     return (
         <div id="container" className='bg-[linear-gradient(180deg,#0A2540_0%,#000000_100%)] min-w-full min-h-full p-5 rounded-3xl flex flex-col'>
-            <div id="profile" className='w-full flex flex-col justify-center items-center bg-[#2c2c2c]/30 px-2.5 py-8 rounded-2xl'>
-                <ProfilePic url='/profile_About.jpg' classname='min-h-20 min-w-20 rounded-2xl bg-center'/>
+            <div id="profile" className='w-full flex flex-col justify-center items-center bg-[#2c2c2c]/30 px-2.5 py-8 rounded-2xl gap-1'>
+                <ProfilePic url='/profile_About.jpg' classname='min-h-20 min-w-20 rounded-2xl bg-center' />
                 <span className='text-text-light font-extrabold text-xl text-center'>Pratyush Chitrakar</span>
                 <span className='text-text-muted text-[0.67rem] ms:text-[0.9rem] ls:text-[1rem] text-center'>Web Developer | UI/UX Enthusiast | CSIT Student</span>
+                <div id="tech_stack" className='w-full flex justify-center gap-3 items-center'>
+                    {icons.map((item)=>
+                <img src={item.icon} alt={`${item.icon}`} key={`${item.icon}`} className='w-3 ms:w-5 h-auto'/>    
+                )}
+                </div>
             </div>
         </div>
     )
