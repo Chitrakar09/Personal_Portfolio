e67f48b03a93876649b7b153e6a7241df7a68e5e
Chitrakar09 <chitrakarpratyush@gmail.com>
2025-10-23 10:57:07 +0545

    made a footer for the website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div id="footer" className='w-full bg-background-dark rounded-[1.25rem] flex justify-between items-center text-text-light'>
+      <div id="copyright" className='flex flex-col items-start justify-evenly'>
+        <span className='font-bold text-sm lg:text-lg'>Pratyush Chitrakar</span>
+        <span className='text-[0.625rem] lg:text-[0.7rem] flex flex-wrap'>© 2025 • Designed & Developed by Pratyush Chitrakar</span>
+      </div>
+      <div id="follow" className='flex flex-col md:flex-row md:justify-end md:items-center items-end justify-center w-full md:gap-2'>
+        <span className='text-[0.75rem] font-bold md:text-lg lg:text-xl'>Follow</span>
+        <span className='text-[0.625rem] md:text-sm lg:text-lg hover:text-accent-amber hover:-translate-y-0.5 transition ease-in-out'><a href=''>Github</a></span>
+        <span className='text-[0.625rem] md:text-sm lg:text-lg hover:text-accent-amber hover:-translate-y-0.5 transition ease-in-out'><a href=''>Linkedin</a></span>
+        <span className='text-[0.625rem] md:text-sm lg:text-lg hover:text-accent-amber hover:-translate-y-0.5 transition ease-in-out'><a href=''>Instagram</a></span>
+      </div>
+    </div>
   )
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,7 @@ import { Footer, Header } from '@/components'
 
 function MainPage() {
   return (
-    <div id='container' className='py-5 px-10 md:px-10'>
+    <div id='container' className='py-5 px-10 flex flex-col justify-between items-center min-h-full min-w-full'>
     <Header/>
 
     <Footer/>
